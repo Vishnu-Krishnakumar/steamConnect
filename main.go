14d389a677502e678be69cf1e1154f84a433d1d6
Vishnu-Krishnakumar <Vishnukkumar2389@gmail.com>
2023-09-13 17:34:55 -0400

    Added http server functionality

--- main.go
+++ main.go
@@ -3,38 +3,48 @@ package main
 import (
 	"booking-app/Desktop/SteamConnect/steam"
 	"fmt"
-	"os"
+	"net/http"
 )
 
+func headers(w http.ResponseWriter, req *http.Request){
+   for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
 
 func main(){
 
-  apiKey := os.Getenv("STEAM_API_KEY")
-  steamClient := steam.NewClient(apiKey)
-  ownedGames, err := steamClient.GetOwnedGames("76561197984646227")
-  friends := steam.NewKey(apiKey)
-  friendsList, err2 := friends.GetFriendsList("76561197984646227")
+  http.HandleFunc("/callOwnedGames",steam.CallGameList)
+  http.HandleFunc("/headers", headers)
+  http.ListenAndServe(":8080", nil)
+  // apiKey := os.Getenv("STEAM_API_KEY")
+  // steamClient := steam.NewClient(apiKey)
+  // ownedGames, err := steamClient.GetOwnedGames("76561197984646227")
+  // friends := steam.NewKey(apiKey)
+  // friendsList, err2 := friends.GetFriendsList("76561197984646227")
 
-  if err != nil{
-	 fmt.Printf("error: %v", err)
-  }
+  // // if err != nil{
+	// //  fmt.Printf("error: %v", err)
+  // // }
 
-   for _, game := range ownedGames.Games{
+  // //  for _, game := range ownedGames.Games{
    
-    name, _ := steam.GetApp(game.AppId)
-    fmt.Printf("app ID: %d\napp name: %s\n", game.AppId, name)
+  // //   name, _ := steam.GetApp(game.AppId)
+  // //   fmt.Printf("app ID: %d\napp name: %s\n", game.AppId, name)
     
-  }
-  if err2 != nil{
-    fmt.Printf("error: %v", err2)
-   }
+  // // }
+  // if err2 != nil{
+  //   fmt.Printf("error: %v", err2)
+  //  }
  
-     for _, friend := range friendsList.Friends{
+  //    for _, friend := range friendsList.Friends{
     
       
-      fmt.Printf("friend: %v %s %v \n",friend.SteamId,friend.Relationship,friend.FriendSince )
+  //     fmt.Printf("friend: %v %s %v \n",friend.SteamId,friend.Relationship,friend.FriendSince )
      
-    }
+  //   }
   
 }
 
--- steam/http-servers.go
+++ steam/http-servers.go
@@ -1,1 +1,26 @@
 package steam
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func CallGameList(w http.ResponseWriter, req *http.Request) {
+
+	apiKey := os.Getenv("STEAM_API_KEY")
+	steamClient := NewClient(apiKey)
+	ownedGames, err := steamClient.GetOwnedGames("76561197984646227")
+	fmt.Fprintf(w, "hello\n")
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+	}
+
+	for _, game := range ownedGames.Games {
+
+		name, _ := GetApp(game.AppId)
+		fmt.Fprintf(w, "app ID: %d\napp name: %s\n", game.AppId, name)
+
+	}
+
+}
